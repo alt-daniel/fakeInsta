14fe0c110726bed811678525b50974d87c0172ef
D.P.N.P. Altorf <daniel.altorf@hva.nl>
2020-07-28 16:32:29 +0200

    Creates a profile object and setup model for posts

---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-lg-3 col-md-6 p-5">
+            <img src="/img/camera.jpeg" alt="" style="max-width: 175px;" class="rounded-circle">
+        </div>
+        <div class="col-lg-9 col-md-6 pt-5">
+
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="#">Add New Post</a>
+            </div>
+            <div class="d-flex">
+
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>
+        </div>
+    </div>
+    
+    <div class="row pt-5 image-row">
+        <div class="col-4 image-square"><img src="/img/stock1.jpg" class="w-100 img-thumbnail"></div>
+        <div class="col-4 image-square"><img src="/img/stock1.jpg" class="w-100 img-thumbnail"></div>
+        <div class="col-4 image-square"><img src="/img/stock1.jpg" class="w-100 img-thumbnail"></div>
+    </div>
+</div>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Instagram
+                    Danigram
                 </div>
 
                 <div class="links">
